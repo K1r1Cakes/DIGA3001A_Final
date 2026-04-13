b96af87ce1af992fc2d238f90187387f6cfd1b22
K1r1Cakes <akiriachetty4@gmail.com>
2026-04-13 23:41:25 +0200

    Main | Imported TMPro and created simple pick up script

---
+++ DIGA3001A_Final/Assets/Scripts/Pickup.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.CompareTag("Player"))
+        {
+            Debug.Log("Picked up");
+            gameObject.SetActive(false);
+        }
+    }
+}
