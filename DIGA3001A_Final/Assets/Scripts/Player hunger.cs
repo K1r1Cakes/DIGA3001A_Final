7f4fff40e86b72789f9ff0ce94b1b4a53d624c88
K1r1Cakes <akiriachetty4@gmail.com>
2026-04-13 23:17:11 +0200

    Main | hunger goes down over time

--- DIGA3001A_Final/Assets/Scripts/Player hunger.cs
+++ DIGA3001A_Final/Assets/Scripts/Player hunger.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Playerhunger : MonoBehaviour
 {
     public float hungerTimer = 5f;
     public float globalTimer;
+    public float playerHunger = 3f;
+    public Image[] hungerImage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,22 +20,47 @@ public class Playerhunger : MonoBehaviour
 
         if(globalTimer <= 0)
         {
-            loseHunger();
-            globalTimer = hungerTimer;
+            if (playerHunger > 0)
+            {
+              loseHunger();
+            globalTimer = hungerTimer;  
+            }
+            else
+            {
+             Debug.Log("Player starved to death");
+             globalTimer = 0;
+            }
+            
         }
+        
         
     }
 
-    public void loseHunger()
+    void loseHunger()
     {
-       
-        
-             Debug.Log("Half hunger bar");
-        
+        playerHunger -= 0.5f;
+        hungerFill();
+    }
+    void hungerFill()
+    {
+        Debug.Log("Half hunger bar");
 
-       
+        for(int i = 0; i < hungerImage.Length; i++)
+        {
+            float remainingHunger = playerHunger - i;
 
-        
-        
+            if (remainingHunger >= 1)
+            {
+                hungerImage[i].fillAmount = 1f;
+            }
+            else if(remainingHunger > 0)
+            {
+                hungerImage[i].fillAmount = 0.5f;
+            }
+            else
+            {
+                hungerImage[i].fillAmount =0f;
+            }
+        }
     }
 }
