1d0a0219f537e6a064beea3f6b09fb8d12263d2f
K1r1Cakes <akiriachetty4@gmail.com>
2026-04-13 22:44:42 +0200

    Main | initial hunger script

--- DIGA3001A_Final/Assets/Scripts/Player hunger.cs
+++ DIGA3001A_Final/Assets/Scripts/Player hunger.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class Playerhunger : MonoBehaviour
 {
+    public float hungerTimer = 5f;
+    public float globalTimer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        globalTimer = hungerTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
+        globalTimer -= Time.deltaTime;
+
+        if(globalTimer <= 0)
+        {
+            loseHunger();
+            globalTimer = hungerTimer;
+        }
+        
+    }
+
+    public void loseHunger()
+    {
+       
+        
+             Debug.Log("Half hunger bar");
+        
+
+       
+
+        
         
     }
 }
