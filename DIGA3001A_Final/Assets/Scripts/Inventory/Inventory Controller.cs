13f3778f3a4459d443abe66de8c004a01a297295
K1r1Cakes <akiriachetty4@gmail.com>
2026-04-26 23:50:39 +0200

    Island | Inventory save data

--- DIGA3001A_Final/Assets/Scripts/Inventory/Inventory Controller.cs
+++ DIGA3001A_Final/Assets/Scripts/Inventory/Inventory Controller.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
+    private ItemDictionary itemDictionary;
     public GameObject inventoryPanel;
     public GameObject slotPrefab;
     public int slotCount;
@@ -9,18 +12,67 @@ public class InventoryController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 0; i < slotCount; i++)
-        {
-          Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
-          if (i < itemPrefabs.Length)
-            {
-                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
-                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                slot.currentItem = item;
-            }
-        }
+        itemDictionary = FindObjectOfType<ItemDictionary>();
+
+        // for(int i = 0; i < slotCount; i++)
+        // {
+        //   Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
+        //   if (i < itemPrefabs.Length)
+        //     {
+        //         GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+        //         item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        //         slot.currentItem = item;
+        //     }
+        // }
     }
 
+    public List<InventorySaveData> GetInventoryItems()
+    {
+        List<InventorySaveData> invData = new List<InventorySaveData>();
+        foreach(Transform slotTransform in inventoryPanel.transform)
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot.currentItem != null)
+            {
+                Item item = slot.currentItem.GetComponent<Item>();
+                invData.Add(new InventorySaveData { itemID = item.itemID, slotIndex = slotTransform.GetSiblingIndex()});
+            }
+        }
+        return invData;
+    }
+
+    public void SetInventoryItems(List<InventorySaveData> inventorySaveData)
+    {
+        //Clear inventory panel - avoid duplicates
+        foreach (Transform child in inventoryPanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        //Create new slots
+        for (int i = 0; i < slotCount; i++)
+        {
+            Instantiate(slotPrefab, inventoryPanel.transform);
+        }
+
+        //Populate with saved items
+        foreach(InventorySaveData data in inventorySaveData)
+        {
+             if (data.slotIndex < slotCount)
+            {
+                Slot slot = inventoryPanel.transform.GetChild(data.slotIndex).GetComponent<Slot>();
+                GameObject itemPrefab = itemDictionary.GetItemPrefab(data.itemID);
+                if (itemPrefab != null)
+                {
+                    GameObject item = Instantiate(itemPrefab, slot.transform);
+                    item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                    slot.currentItem = item;
+                }
+            }
+        }
+       
+
+    }
     public bool AddItem(GameObject itemPrefab)
     {
         //Look for empty slot
--- DIGA3001A_Final/Assets/Scripts/Save Data.cs
+++ DIGA3001A_Final/Assets/Scripts/Save Data.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
 public class SaveData 
 {
    public Vector3 playerPosition;
+   public List<InventorySaveData> inventorySaveData;
 }
