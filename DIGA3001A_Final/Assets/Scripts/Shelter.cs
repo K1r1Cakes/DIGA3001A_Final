1260a7e2c28ecbcff00b041d259dcaa9c455b0eb
K1r1Cakes <akiriachetty4@gmail.com>
2026-04-29 12:17:32 +0200

    Trying to fix labels

---
+++ DIGA3001A_Final/Assets/Scripts/Shelter.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class Shelter : MonoBehaviour
+{
+    public TextMeshProUGUI shelterText;
+    public float delay = 3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collide)
+    {
+        if (collide.CompareTag("Player"))
+        {
+            shelterText.gameObject.SetActive(true);
+            StartCoroutine(Hide());
+        }
+    }
+
+    IEnumerator Hide()
+    {
+        yield return new WaitForSeconds(delay);
+        shelterText.gameObject.SetActive(false);
+    }
+}
