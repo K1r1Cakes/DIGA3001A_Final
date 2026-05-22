ea66c68fc132c3f5f02a575f6b2b622d81847bfe
K1r1Cakes <akiriachetty4@gmail.com>
2026-05-22 23:09:45 +0200

    Island | Commeneted out save inventory code

--- DIGA3001A_Final/Assets/Scripts/Inventory/Inventory Controller.cs
+++ DIGA3001A_Final/Assets/Scripts/Inventory/Inventory Controller.cs
@@ -55,20 +55,20 @@ public class InventoryController : MonoBehaviour
         }
 
         //Populate with saved items
-        foreach(InventorySaveData data in inventorySaveData)
-        {
-             if (data.slotIndex < slotCount)
-            {
-                Slot slot = inventoryPanel.transform.GetChild(data.slotIndex).GetComponent<Slot>();
-                GameObject itemPrefab = itemDictionary.GetItemPrefab(data.itemID);
-                if (itemPrefab != null)
-                {
-                    GameObject item = Instantiate(itemPrefab, slot.transform);
-                    item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                    slot.currentItem = item;
-                }
-            }
-        }
+        // foreach(InventorySaveData data in inventorySaveData)
+        // {
+        //      if (data.slotIndex < slotCount)
+        //     {
+        //         Slot slot = inventoryPanel.transform.GetChild(data.slotIndex).GetComponent<Slot>();
+        //         GameObject itemPrefab = itemDictionary.GetItemPrefab(data.itemID);
+        //         if (itemPrefab != null)
+        //         {
+        //             GameObject item = Instantiate(itemPrefab, slot.transform);
+        //             item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        //             slot.currentItem = item;
+        //         }
+        //     }
+        // }
        
 
     }
