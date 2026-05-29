2c25c1b9d9ad6357d9f198b2698b1a9455f00977
K1r1Cakes <akiriachetty4@gmail.com>
2026-05-29 13:23:02 +0200

    Made script

---
+++ DIGA3001A_Final/Assets/Scripts/ForestEnter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ForestEnter : MonoBehaviour
+{
+   //If you dont have that item then you cant enetr
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
