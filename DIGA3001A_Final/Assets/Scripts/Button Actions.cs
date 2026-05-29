16ee18fd83ad70fe745a00a6e718acbec07f925c
K1r1Cakes <akiriachetty4@gmail.com>
2026-05-29 11:14:46 +0200

    Intro | Intro dialogue

--- DIGA3001A_Final/Assets/Scripts/Button Actions.cs
+++ DIGA3001A_Final/Assets/Scripts/Button Actions.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ButtonActions : MonoBehaviour
 {
+    private Dialogue dialogue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
---
+++ DIGA3001A_Final/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        dialogueText.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            SceneManager.LoadScene("Island");
+            
+        }
+    }
+}
