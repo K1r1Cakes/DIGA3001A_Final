88ef47805c69211da84eec8b0456c07ac92a85e9
K1r1Cakes <akiriachetty4@gmail.com>
2026-05-31 21:35:51 +0200

    Island | Edited Trade and Inventory UI

--- DIGA3001A_Final/Assets/Scripts/Trade/Trade.cs
+++ DIGA3001A_Final/Assets/Scripts/Trade/Trade.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using TMPro;
 
 public class Trade : MonoBehaviour
 {
     public ShopItem shopItem;
    public InventoryController inventory;
    public PlayerLevel playerLevel;
+   public TextMeshProUGUI infoText;
 
     public void OnBuyItem()
     {
         if(playerLevel.levelAmount < shopItem.itemCost)
         {
+            infoText.text = "Not enough Orbs";
             return;
         }
         else
@@ -18,6 +21,25 @@ public class Trade : MonoBehaviour
             playerLevel.loseLevel(shopItem.itemCost);
         }
 
+        if (shopItem.itemName == "Stick")
+        {
+            infoText.text = "Place the stick in the hotbar and select it by using teh corresponding number.";
+        }
+
+        if (shopItem.itemName == "Water")
+        {
+            infoText.text = "Use this item to add +10 thirst.";
+        }
+
+        if (shopItem.itemName == "Medkit")
+        {
+            infoText.text = "Use this item to +10 health";
+        }
+
+        if (shopItem.itemName == "Coconut")
+        {
+            infoText.text = "Use this item to +5 coconuts.";
+        }
 
     }
 }
